7dd52a5ed71671c57067cc36adb02d8b4ebc0555
Eugene Pavlov <bjohn.ext@gmail.com>
2015-12-12 09:01:58 +0300

    first commit

---
+++ Assets/Character2D.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Character2D : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpForce = 400f;
+
+    private Rigidbody2D rigidbody;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("space key was pressed");
+            rigidbody.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+}
